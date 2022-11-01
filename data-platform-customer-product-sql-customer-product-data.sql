81f51164fddd57246e2eaeaac7037cc1b23dd185
MiyuYokoyama0109 <miyu.y@latona.io>
2022-11-01 17:15:43 +0900

    release

--- data-platform-customer-product-sql-customer-product-data.sql
+++ data-platform-customer-product-sql-customer-product-data.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `data_platform_customer_product_customer_product_data`
 (
-  `BusinessPartner`                   varchar(10) NOT NULL,       --新規追加
-  `Customer`                          varchar(10) NOT NULL,  
+  `BusinessPartner`                   int(10) NOT NULL,          -- 新規追加
+  `Customer`                          int(10) NOT NULL,  
   `SalesOrganization`                 varchar(4) NOT NULL,
   `DistributionChannel`               varchar(2) NOT NULL,
-  `Product`                           varchar(40) NOT NULL,　　   --名称変更
-  `ProductByCustomer`                 varchar(40) DEFAULT NULL,   --名称変更
-  `ProductDescriptionByCustomer`      varchar(100) DEFAULT NULL,  --名称変更
+  `Product`                           varchar(40) NOT NULL,　　   -- 名称変更
+  `ProductByCustomer`                 varchar(40) DEFAULT NULL,   -- 名称変更
+  `ProductDescriptionByCustomer`      varchar(100) DEFAULT NULL,  -- 名称変更
   `BaseUnit`                          varchar(3) DEFAULT NULL,
   `SalesUnit`                         varchar(3) DEFAULT NULL,
     PRIMARY KEY (`BusinessPartner`, `Customer`, `SalesOrganization`, `DistributionChannel`, `Product`)
